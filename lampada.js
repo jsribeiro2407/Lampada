7f657c56edfcf96b380d65a3e3405441de046b73
jsribeiro2407 <js.ribeiro@discente.ufma.br>
2024-04-02 15:49:31 -0300

    Versão inicial

---
+++ lampada.js
@@ -0,0 +1,23 @@
+var lamp = window.document.getElementById('lampada')
+
+function estaquebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function ligar(){
+    if(!estaquebrada()){
+      lamp.src = 'ligada.svg'  
+    }
+    
+}
+
+function desligar(){
+    if(!estaquebrada()){
+     lamp.src = 'desligada.svg'
+    }
+}
+   
+lamp.addEventListener('click', quebrar)
+function quebrar(){
+    lamp.src = 'quebrada.svg'
+}
